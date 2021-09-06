dfdfb7e7071dd53e8b2dabe8c9360eb978c465ac
mohamed <moh.essa2020@gmail.com>
2021-09-06 14:54:37 +0200

    edit modal

--- js/file.js
+++ js/file.js
@@ -54,3 +54,10 @@ $( function() {
   // $( "#amount" ).val( "kms" + $( "#slider-range" ).slider( "values", 0 ) +
   // " - kms" );
 } );
+$(document).on('show.bs.modal', '.modal', function () {
+  var zIndex = 1040 + (10 * $('.modal:visible').length);
+  $(this).css('z-index', zIndex);
+  setTimeout(function() {
+      $('.modal-backdrop').not('.modal-stack').css('z-index', zIndex - 1).addClass('modal-stack');
+  }, 0);
+});
